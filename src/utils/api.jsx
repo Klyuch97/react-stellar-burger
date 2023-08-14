71cc388cd8b060240422e4c13fe6f70e57e44ef3
Nikita Klyuchnikov <klyuch1994@mail.ru>
2023-08-14 10:46:34 +0500

    check bug

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 export const BASE_URL = 'https://norma.nomoreparties.space/api/';
 
 const checkResponse = (res) => {
-    if (!res.ok) {
+    if (res.ok) {
       return res.json();
     }
     return Promise.reject(`Ошибка ${res.status}`);
