4fe150b19e2a2d6471417ea56a2c9b66d8bc0417
Nikita Klyuchnikov <klyuch1994@mail.ru>
2023-09-05 14:45:34 +0500

    сделал header Link

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import headerStyles from '../header/header.module.css'
+import headerStyles from '../header/header.module.css';
+import { Link, NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+    const location = useLocation();
     return (
         <header className={headerStyles.header}>
             <div className={`${headerStyles.content} mt-4 mb-4`}>
                 <div className={headerStyles.listItem}>
-                    <a href='#' className={`${headerStyles.navigation} ml-5 mr-5`}>
-                        <BurgerIcon type="primary" />
-                        <p className="pl-2 text text_type_main-default">Конструктор</p>
-                    </a>
+                    <Link to="/" className={`${headerStyles.navigation} ml-5 mr-5`}>
+                        <BurgerIcon type={location.pathname === "/" ? "primary" : "secondary"} />
+                        <p className={`pl-2 text text_type_main-default text_color_${location.pathname === "/" ? "active" : "inactive"}`}
+                        >Конструктор</p>
+                    </Link>
                     <a href='#' className={`${headerStyles.navigation} ml-5 mr-5`}>
                         <ListIcon type="secondary" />
                         <p className="pl-2 text text_type_main-default text_color_inactive">Лента заказов</p>
@@ -19,10 +23,11 @@ const Header = () => {
                 <div className={headerStyles.logo}>
                     <Logo></Logo>
                 </div>
-                <a href='#' className={`${headerStyles.navigation} ml-5 mr-5`}>
-                    <ProfileIcon type="secondary" />
-                    <p href='#' className="pl-2 text text_type_main-default text_color_inactive">Личный кабинет</p>
-                </a>
+                <Link to="/profile" className={`${headerStyles.navigation} ml-5 mr-5`}>
+                    <ProfileIcon type={location.pathname === "/profile" ? "primary" : "secondary"} />
+                    <p className={`pl-2 text text_type_main-default text_color_${location.pathname === "/profile" ? "active" : "inactive"}`}>
+                        Личный кабинет</p>
+                </Link>
 
             </div>
         </header>
