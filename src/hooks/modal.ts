5c8b5b1d9e26d7400fb5155074d744c8384325d5
Nikita Klyuchnikov <klyuch1994@mail.ru>
2023-10-25 11:26:00 +0500

    hooks with TS

--- src/hooks/modal.ts
+++ src/hooks/modal.ts
@@ -1,15 +1,15 @@
 import { useState, useCallback } from "react";
 
-// кастомные хуки всегда должны начинаться с глагола `use`, чтобы реакт понял, что это хук. Он следит за их вызовами
+
 export const useModal = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   // `useCallback` нужен для того, чтобы зафиксировать ссылку на функцию. Таким образом уменьшится кол-во перерисовок компонента, куда будет передана эта функция
-  const openModal = useCallback(() => {
+  const openModal = useCallback((): void => {
     setIsModalOpen(true);
   }, []);
 
-  const closeModal = useCallback(() => {
+  const closeModal = useCallback((): void => {
     setIsModalOpen(false);
   }, []);
 
