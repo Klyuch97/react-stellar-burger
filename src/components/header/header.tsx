bd369e71a0e2639b2131b80aee3b1c87ddc466b8
Nikita Klyuchnikov <klyuch1994@mail.ru>
2023-11-07 12:05:16 +0500

    исправил header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,12 +3,14 @@ import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-dev
 import headerStyles from '../header/header.module.css';
 import { Link, NavLink } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from '../../services/hooks';
 
 const Header = () => {
     const location = useLocation();
-    const isActive:boolean = location.pathname === "/profile" || location.pathname === "/profile/orders";
-    const isActiveLenta:boolean = location.pathname === "/feed";
-    
+    const isActive: boolean = location.pathname === "/profile" || location.pathname === "/profile/orders";
+    const isActiveLenta: boolean = location.pathname === "/feed";
+    const { user } = useSelector(state => state.user);
+
     return (
         <header className={headerStyles.header}>
             <div className={`${headerStyles.content} mt-4 mb-4`}>
@@ -29,8 +31,9 @@ const Header = () => {
                 </div>
                 <Link to="/profile" className={`${headerStyles.navigation} ml-5 mr-5`}>
                     <ProfileIcon type={isActive ? "primary" : "secondary"} />
-                    <p className={`pl-2 text text_type_main-default text_color_${isActive ? "active" : "inactive"}`}>
-                        Личный кабинет</p>
+                    {user === null ? <p className={`pl-2 text text_type_main-default text_color_inactive`}>
+                        Личный кабинет</p> : <p className={`pl-2 text text_type_main-default text_color_active ${headerStyles.activeUser}`}>
+                        {user.name}</p>}
                 </Link>
 
             </div>
